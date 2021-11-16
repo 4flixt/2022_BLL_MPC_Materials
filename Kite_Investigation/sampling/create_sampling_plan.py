53d3f5569e5cbbab7b502df7b71bea4715243fc9
Felix-Mac <felix.fiedler@tu-berlin.de>
2021-11-16 17:56:19 +0100

    Sample training data

---
+++ Kite_Investigation/sampling/create_sampling_plan.py
@@ -0,0 +1,69 @@
+import sys
+sys.path.append('../../../do-mpc_fork/')
+import do_mpc
+import time
+import os
+
+import numpy as np
+import pdb
+
+def main():
+
+    sp = do_mpc.sampling.SamplingPlanner()
+    sp.data_dir = './kite_sampling_01/'
+    sp.set_param(overwrite=True)
+
+    def gen_x0():
+        lb_theta = 0
+        lb_phi = -0.5*np.pi
+        lb_psi = -1.0*np.pi
+
+        ub_theta = 0.5*np.pi
+        ub_phi = 0.5*np.pi
+        ub_psi = 1.0*np.pi
+
+        # with mean and radius:
+        m_theta, r_theta = (ub_theta+lb_theta)/2, (ub_theta-lb_theta)/2
+        m_phi, r_phi = (ub_phi+lb_phi)/2, (ub_phi-lb_phi)/2
+        m_psi, r_psi = (ub_psi+lb_psi)/2, (ub_psi-lb_psi)/2
+        # How close can the intial state be to the bounds?
+        # tightness=1 -> Initial state could be on the bounds.
+        # tightness=0 -> Initial state will be at the center of the feasible range.
+        tightness = 0.6
+        theta_0 = m_theta-tightness*r_theta+2*tightness*r_theta*np.random.rand()
+        phi_0 = m_phi-tightness*r_phi+2*tightness*r_phi*np.random.rand()
+        psi_0 = m_psi-tightness*r_psi+2*tightness*r_psi*np.random.rand()
+
+        x0 = np.array([theta_0, phi_0, psi_0]).reshape(-1,1)
+
+        return x0
+
+    # Add variables
+    sp.set_sampling_var('w_ref', lambda: np.random.uniform(6,16))
+    sp.set_sampling_var('E_0', lambda: np.random.uniform(5,8))
+    sp.set_sampling_var('h_min', lambda: np.random.uniform(80,120))
+    sp.set_sampling_var('seed', lambda: np.random.randint(1,1000))
+    sp.set_sampling_var('x0', gen_x0)
+
+
+    plan = sp.gen_sampling_plan(n_samples = 100)
+
+    sp.export('kite_sampling_01_plan')
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+# pool = mp.Pool(processes=4)
+# res = [pool.apply_async(sampler.sample_data, args=()) for k in range(4)]
+# out = [p.get() for p in res]
+
+# dh = do_mpc.sampling.DataHandler(plan)
+#
+# dh.set_post_processing('res_1', lambda x: x)
+# dh.set_post_processing('res_2', lambda x: x**2)
+#
+#
+# res = dh[:]
---
+++ Kite_Investigation/sampling/sample_system.py
@@ -0,0 +1,73 @@
+import sys
+sys.path.append('../../../do-mpc_fork/')
+import do_mpc
+
+import time
+import os
+
+import numpy as np
+import pdb
+import multiprocessing as mp
+from functools import partial
+from do_mpc.tools import load_pickle
+
+
+sys.path.append('../system/')
+from template_model import template_model
+from template_mpc import template_mpc
+from template_simulator import template_simulator
+
+def info():
+    print(mp.current_process())
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+
+
+def sample_function(x0, w_ref, E_0, h_min, seed):
+    info()
+    model = template_model()
+    mpc = template_mpc(model, w_ref, E_0, h_min=h_min)
+    simulator = template_simulator(model, w_ref, E_0)
+    estimator = do_mpc.estimator.StateFeedback(model)
+
+
+    # set initial values and guess
+    mpc.x0 = x0
+    simulator.x0 = x0
+    estimator.x0 = x0
+
+    mpc.set_initial_guess()
+
+    n_steps = 200
+
+    for k in range(n_steps):
+        try:
+            u0 = mpc.make_step(x0)
+            y_next = simulator.make_step(u0)
+            x0 = estimator.make_step(y_next)
+        except:
+            break
+
+    return simulator.data
+
+
+
+def main():
+    plan = load_pickle('./kite_sampling_01/kite_sampling_01_plan.pkl')
+
+    sampler = do_mpc.sampling.Sampler(plan)
+    sampler.set_param(print_progress = False)
+    sampler.data_dir = './kite_sampling_01/sample_results/'
+
+
+    sampler.set_sample_function(sample_function)
+
+    with mp.Pool(processes=8) as pool:
+        p = pool.map(sampler.sample_idx, list(range(sampler.n_samples)))
+
+    #sampler.sample_idx(0)
+
+
+
+if __name__ == '__main__':
+    main()
